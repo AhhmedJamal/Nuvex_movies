c9b059cb26047fcfdc1e3b9dbc7be532a6ad6d2c
AhhmedJamal <ahhmed.jamal@gmail.com>
2024-04-20 23:08:45 +0200

    update auth page and home page

--- src/pages/MyListPage.tsx
+++ src/pages/MyListPage.tsx
@@ -9,16 +9,19 @@ function MyListPage() {
     try {
       const user = auth.currentUser;
       if (user) {
-        const docRef = doc(db, "users", user.email);
+        const docRef = doc(db, "users", user.email || "");
         const docSnapshot = await getDoc(docRef);
         const userData = docSnapshot.data();
-        setDataMyList(userData.myList);
+        if (userData) {
+          setDataMyList(userData.myList);
+        } else {
+          console.log("User data is undefined");
+        }
       } else {
-        console.log("Document does not exist");
+        console.log("User is not authenticated");
       }
     } catch (error) {
       console.error("Error fetching document:", error);
-      // You can log the full error object for more details: console.error(error);
     }
   };
   useEffect(() => {
@@ -31,7 +34,7 @@ function MyListPage() {
     }, 1000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div>MyListPage</div>;
+  return <div>{isLoading ? <h1>true</h1> : <h1>false</h1>}</div>;
 }
 
 export default MyListPage;
--- src/types/UserProps.ts
+++ src/types/UserProps.ts
@@ -1,9 +1,8 @@
 export interface UserData {
-  uid?: string;
-  name?: string | null;
-  email?: string | null;
-  photoURL?: string | null;
-  displayName?: string | null;
-  myList?: [] | null;
+  uid: string;
+  name: string | null;
+  email: string | null;
+  photoURL: string | null;
+  displayName: string | null;
+  myList: [] | null;
 }
-
