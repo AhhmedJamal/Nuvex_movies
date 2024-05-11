22798bfa99fbe6eb751879099d2b710844396e5a
AhhmedJamal <ahhmed.jamal@gmail.com>
2024-05-12 02:10:59 +0300

    change font

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,10 +2,8 @@ import { logoProps } from "../types/LogoProps";
 
 function Logo({ fontSize, heightBlur, bottom }: logoProps) {
   return (
-    <div
-      className={`relative w-fit  ${fontSize} font-[700] text-primary overflow-hidden z-10`}
-    >
-      <p>NUVEX</p>
+    <div className={`relative w-fit overflow-hidden z-10`}>
+      <h1 className={`${fontSize} font-[700] text-primary`}>NUVEX</h1>
       <span
         className={`absolute ${bottom} w-full ${heightBlur} right-[-1px] bg-[#ffffff0c] [box-shadow:0_4px_30px_#00000005] backdrop-filter backdrop-blur-[2px]`}
       ></span>
